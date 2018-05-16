bb50c600c92b68dd78105953178fc7d444566f58
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-05-15 22:58:46 -0500

    Working with Avi’s strategy for alignCrossModal2, using iterative use of brain masks based on Freesurfer “orig”.

--- src/+mlfourdfp/VariableMaskT4ResolveBuilder.m
+++ src/+mlfourdfp/VariableMaskT4ResolveBuilder.m
@@ -15,6 +15,8 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
         %% OVERRIDES
 		  
         function [ipr,this]   = revise(this, ipr)
+            ipr.logPath = fullfile(this.sessionData.vallLocation, 'Log', '');
+            ensuredir(ipr.logPath);
             ipr = this.copySourceToDest(ipr); % crop/copy ipr.source to ipr.dest       
             this.imageRegLog = loggerFilename( ...
                 ipr.dest{this.indexOfReference}, ...
@@ -29,7 +31,24 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
                 'func', 'VariableMaskT4ResolveBuilder',...
                 'path', ipr.logPath);
             
-            this = this.imageReg(ipr);
+            ipr.cacheLoc = ensuredir( ...
+                fullfile(this.sessionData.vallLocation, ['VarMaskT4RBCache_' datestr(now, 30)]));
+            ipr.maskForImages = 'none';
+            ipr.useMetricGradient = false;
+            this = this.imageReg(ipr);  
+            
+            ipr.cacheLoc = ensuredir( ...
+                fullfile(this.sessionData.vallLocation, ['VarMaskT4RBCache_' datestr(now, 30)]));
+            ipr.maskForImages = 'Msktgen';
+            ipr.useMetricGradient = true;
+            this = this.imageReg(ipr);  
+            
+            ipr.cacheLoc = ensuredir( ...
+                fullfile(this.sessionData.vallLocation, ['VarMaskT4RBCache_' datestr(now, 30)]));
+            ipr.maskForImages = 'Msktgen';
+            ipr.useMetricGradient = true;
+            this = this.imageReg(ipr); 
+            
             [ipr,~,this] = this.resolveAndPaste(ipr); 
             this.teardownRevision(ipr);
             this.rnumber = this.rnumber + 1;
@@ -37,7 +56,7 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
         function this =         imageReg(this, ipr)
             stagedImgs  = this.lazyStageImages(ipr);    % contracted wrt this.indicesLogical
             blurredImgs = this.lazyBlurImages(ipr);     % "
-            maskedImgs  = this.lazyMasksForImages(ipr); % "
+            maskedImgs  = this.lazyMasksForImages(ipr, stagedImgs); % "
             assertSizeEqual(stagedImgs, blurredImgs, maskedImgs);
             len = sum(this.indicesLogical);
             t4Failures = zeros(len, len);
@@ -46,26 +65,20 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
                     if (m ~= n)
                         try
                             t4 = this.buildVisitor.filenameT4(stagedImgs{n}, stagedImgs{m});
-                            if (~lexist(t4))
-                                this.buildVisitor.align_crossModal( ...
-                                    'dest',       blurredImgs{m}, ...
-                                    'source',     blurredImgs{n}, ...
-                                    'destMask',   'none', ...
-                                    'sourceMask', 'none', ...
-                                    't4',         t4, ...
-                                    't4img_4dfp', false, ...
-                                    'useMetricGradient', false, ...
-                                    'log',        this.imageRegLog);
-                                this.buildVisitor.align_crossModal( ...
-                                    'dest',       blurredImgs{m}, ...
-                                    'source',     blurredImgs{n}, ...
-                                    'destMask',   maskedImgs{m}, ...
-                                    'sourceMask', 'none', ...
-                                    't4',         t4, ...
-                                    't4img_4dfp', false, ...
-                                    'useMetricGradient', true, ...
-                                    'log',        this.imageRegLog);
-                            end
+                            
+                            %% CONTINUALLY UPDATE T4
+
+                            this.buildVisitor.align_crossModal( ...
+                                'dest',       blurredImgs{m}, ...
+                                'source',     blurredImgs{n}, ...
+                                'destMask',   maskedImgs{m}, ...
+                                'sourceMask', 'none', ...
+                                't4',         t4, ...
+                                't4img_4dfp', false, ...
+                                'useMetricGradient', ipr.useMetricGradient, ...
+                                'log',        this.imageRegLog);
+                            copyfile(t4, ipr.cacheLoc);
+                            
                             % t4_resolve requires an idiomatic naming convention for t4 files,
                             % based on the names of image files
                             % e. g., image1_to_image2_t4    
@@ -114,6 +127,7 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
                     end
                 end
             end 
+            this.appendVMRBLog([datestr(now) '\n']);
             this.appendVMRBLog( ...
                 sprintf('VariableMaskT4ResolveBuilder.assessT4ResolveErr: stagedImgs->%s\n', ...
                 cell2str(stagedImgs, 'AsRow', true)));
@@ -127,7 +141,46 @@ classdef VariableMaskT4ResolveBuilder < mlfourdfp.CompositeT4ResolveBuilder
             end
             mlbash(sprintf('echo \"%s\" >> %s', s, this.vmRBLog));
             fprintf(s);
-        end        
+        end     
+        function fqfps = lazyMasksForImages(this, ipr, stagedImgs, varargin)
+            %  @param ipr.maskForImages is 'none' or the fileprefix/name of an anatomical image which will be 
+            %  thresholded to generate a mask; the anatomical image must have transverse orientation.   
+            %  See also:  mlfourdfp.FourdfpVisitor.transverse_t4.
+            %  @param this.theImages must be populated with the images to be masked.
+            %  @param stagedImgs is cell-arrayed.
+            %  @return fqfps, a cell-array of binary masks, identically sized to this.theImages.
+            
+            fqfps = cell(1, length(ipr.source));            
+            if (~iscell(ipr.maskForImages) && strcmpi(ipr.maskForImages, 'none'))
+                fqfps = cellfun(@(x) 'none', fqfps, 'UniformOutput', false);
+                return
+            end   
+            
+            % use mask for first source for all other sources
+            if (~iscell(ipr.maskForImages))
+                ipr.maskForImages = repmat(ensureCell(ipr.maskForImages), size(ipr.source));
+            end
+            assertSizeEqual(ipr.maskForImages, ipr.source);            
+            ii = 1;  
+            if (strcmp(ipr.maskForImages{ii}, 'Msktgen'))
+                try
+                    mg = mlpet.Msktgen('sessionData', this.sessionData);
+                    mskt = mg.constructMskt( ...
+                        'source', ipr.source{ii}, ...
+                        'intermediaryForMask', this.sessionData.T1001, ...
+                        'sourceOfMask', fullfile(this.sessionData.vLocation, 'brainmask.4dfp.ifh'));
+                    fqfps{ii} = mskt.fqfileprefix;
+                catch ME
+                    handexcept(ME);
+                end
+            end
+            for ii = 2:length(ipr.source)
+                t4 = this.buildVisitor.filenameT4(stagedImgs{ii}, stagedImgs{1}); % from iteration without mask
+                assert(lexist(t4));
+                fqfps{ii} = [ipr.source{ii} '_mskt'];
+                mg.t4img_4dfp(t4, mskt.fqfileprefix, 'out', fqfps{ii}, 'options', ['-O' ipr.source{1}]);
+            end
+        end       
         
  		function this = VariableMaskT4ResolveBuilder(varargin)
  			%% VARIABLEMASKT4RESOLVEBUILDER
