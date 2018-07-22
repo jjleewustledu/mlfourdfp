cc52e6cab9f54d10a614c8f8e7c906a8c8c0e57d
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-07-22 17:18:51 -0500

    Manually tested.

--- src/+mlfourdfp/Viewer.m
+++ src/+mlfourdfp/Viewer.m
@@ -1,15 +1,11 @@
-classdef Viewer 
-	%% VIEWER  
+classdef Viewer < mlfourd.Viewer
+	%% VIEWER prepares viewer apps with mlfourdfp.Fourdfp
 
 	%  $Revision$
  	%  was created 30-Mar-2018 02:27:48 by jjlee,
  	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlfourdfp/src/+mlfourdfp.
  	%% It was developed on Matlab 9.1.0.441655 (R2016b) for MACI64.  Copyright 2018 John Joowon Lee.
- 	
-	properties
- 		app = 'freeview'
-    end
-
+    
     methods (Static)
         function [s,r] = view(varargin)
             % VIEW has this.app := 'freeview'.
@@ -19,85 +15,75 @@ classdef Viewer
         end
     end
     
-	methods 
+    methods
         
-        function [s,r] = aview(this, varargin)
-            targs = cell(size(varargin));
-            for v = 1:length(varargin)
-                targs{v} = this.interpretTarget(varargin{v});
-            end
-            [s,r] = mlbash(sprintf('%s %s', this.app, cell2str(targs, 'AsRow', true)));
-        end
-		  
  		function this = Viewer(varargin)
-            ip = inputParser;
-            addOptional(ip, 'app', 'freeview', @ischar);
-            parse(ip, varargin{:});
-            this.app = ip.Results.app;
+            this = this@mlfourd.Viewer(varargin{:});
  		end
-    end 
+    end
     
-    %% PRIVATE
+    %% PROTECTED
     
-    methods (Access = private)
-        function interp = interpretTarget(this, targ)
+    methods (Access = protected)
+        function fqt = fqtarget(~, t)
+            fqt = [myfileprefix(t) '.4dfp.img'];
+        end
+        function fn = constructTmp4dfp(this, fn)
+            fn_ = tempFqfilename(fn);
+            copyfile(fn, fn_);
+            this.fv.nifti_4dfp_4(fn_); % deletes *.nii[.gz]
+            fn = this.fqtarget(fn_);
+        end
+        function [interp,todel] = interpretTarget(this, targ)
             %  @return interp is intended for use by shell apps.  
             %  It will be char, string or cell.  Consider using cell2str(..., 'AsRow', true).
-            
-            fv = mlfourdfp.FourdfpVisitor;
-            
+                       
             switch (class(targ))
                 case 'cell'
-                    interp = cellfun(@(x) this.interpretTarget(x), targ, 'UniformOutput', false);
+                    [interp,todel] = cellfun(@(x) this.interpretTarget(x), targ, 'UniformOutput', false);
                 case {'char' 'string'}
                     
                     % 4dfp extensions
-                    if (lstrfind(targ, '.4dfp.img'))
-                        interp = targ;
-                        return
-                    end
-                    if (lstrfind(targ, '.4dfp.ifh'))
-                        interp = replace(targ, '.4dfp.ifh', '.4dfp.img');
+                    if (lstrfind(targ, '.4dfp.'))
+                        interp = this.fqtarget(targ);
+                        todel = false;
                         return
                     end
                     
                     % no extension found
                     if (lexist([targ '.4dfp.img'], 'file'))
-                        interp = [targ '.4dfp.img'];
-                        return
-                    end
-                    if (lexist([targ '.nii.gz'], 'file'))
-                        fv.nifti_4dfp_4([targ '.nii.gz']);
-                        interp = [targ '.4dfp.img'];
+                        interp = this.fqtarget(targ);
+                        todel = false;
                         return
                     end
                     if (lexist([targ '.nii'], 'file'))
-                        fv.nifti_4dfp_4([targ '.nii']);
-                        interp = [targ '.4dfp.img'];
+                        interp = this.constructTmp4dfp([targ '.nii']);
+                        todel = true;
+                        return
+                    end
+                    if (lexist([targ '.nii.gz'], 'file'))
+                        interp = this.constructTmp4dfp([targ '.nii.gz']);
+                        todel = true;
                         return
                     end
                     
-                    % NIfTI extension
-                    if (lstrfind(targ, '.nii.gz') && lexist([targ '.nii.gz'], 'file'))
-                        fv.nifti_4dfp_4(targ);                        
-                        interp = replace(targ, '.nii.gz', '.4dfp.img');
-                        return
-                    end
-                    if (lstrfind(targ, '.nii') && lexist([targ '.nii'], 'file'))
-                        fv.nifti_4dfp_4(targ);                        
-                        interp = replace(targ, '.nii', '.4dfp.img');
+                    % NIfTI extensions .nii[.gz]
+                    if (lstrfind(targ, '.nii'))
+                        interp = this.constructTmp4dfp(targ);
+                        todel = true;
                         return
                     end
                     
                     % command-line options
                     interp = targ;
+                    todel = false;
                 otherwise
-                    if (isa(targ, 'mlfourd.ImagingContext') || isa(targ, 'mlfourd.INIfTI'))
+                    if (isa(targ, 'mlfourd.ImagingContext'))
                         if (~lexist(targ.fqfilename))
                             targ.filesuffix = '.4dfp.ifh';
                             targ.save;
                         end
-                        interp = this.interpretTarget(targ.fqfilename);
+                        [interp,todel] = this.interpretTarget(targ.fqfilename);
                         return
                     end
                     error('mlfourdfp:unsupportedSwitchcase', ...
